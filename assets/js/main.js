ce6425f7992eabc36f3a704c50c90d98237b143f
Davi Araujo Cândido <daviaraujocandido@gmail.com>
2023-09-28 15:46:31 -0300

    adding js to modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,7 +14,12 @@ function loadPokemonItems(offset, limit) {
     pokemonList.innerHTML += pokemons
       .map(
         (pokemon) => `
-          <li class="pokemon bg-${pokemon.type}" onclick="openModal()">
+          <li 
+          class="pokemon bg-${pokemon.type}" 
+          onclick="openModal(
+            '${pokemon.type}', '${pokemon.types}', '${pokemon.name}', '${
+          pokemon.photo
+        }')">
             <div class='cardHeader'>
               <span class="name">${pokemon.name}</span>
               <span class="number">#${pokemon.number}</span>
@@ -38,13 +43,50 @@ function loadPokemonItems(offset, limit) {
           </li>`
       )
       .join("");
-    hideLoading();
   });
+  hideLoading();
 }
 
 loadPokemonItems(offset, limit);
 
-function openModal() {
+function openModal(type, types, name, photo) {
+  modal.innerHTML = `
+        <div class="modal-content bg-${type}">
+        <header id="headerTitle">
+          <img
+            src="https://user-images.githubusercontent.com/29473781/180619084-a56960ab-7efa-4e34-9d33-4e3e581d62ff.png"
+            class="pokedexTitle"
+          />
+          <span class="close" onclick="closeModal()">&times;</span>
+        </header>
+        <section class="modal-contents bg-white">
+          <img
+            class="modal-pokemon"
+            src="${photo}"
+            alt="${name}"
+          />
+          <div class="pokemon-info">
+            <ol class="types">
+              <li class="name">
+                <h1>${name}</h1>
+              </li>
+              <div class="flex-center">
+              ${types
+                .split(",")
+                .map(
+                  (typeSlot) =>
+                    `<li class="pokemon-type ${type}">${typeSlot}</li>`
+                )
+                .join("")}
+              </div>
+            </ol>
+            <p class="pokemon-about">
+              There is a plant seed on its back right from the day this
+              Pokémon is born. The seed slowly grows larger.
+            </p>
+          </div>
+        </section>
+      </div>`;
   modal.style.display = "block";
 }
 
